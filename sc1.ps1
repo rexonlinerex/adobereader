3d7428aaa0c7d3ec368a00629a4c3873ef2712a0
rexonlinerex <pc-h5dx2g3@rex-order.online>
2026-08-10 16:53:18 -0700

    Delete sc1.ps1

--- sc1.ps1
+++
@@ -1,6 +0,0 @@
-$url = "?????"
-$msi = "C:\Temp\ScreenConnect.msi"
-mkdir C:\Temp -Force | Out-Null
-Invoke-WebRequest -Uri $url -OutFile $msi -UseBasicParsing
-Start-Process msiexec.exe -ArgumentList "/i `"$msi`" /qb /norestart" -NoNewWindow -Wait
-Remove-Item $msi -Force
